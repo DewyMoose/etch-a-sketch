f16a745f4193d3a3dc3df4bb40452afbf1fcd807
Brenden Dougherty <bdougherty89@yahoo.com>
2025-03-16 22:41:13 -0700

    finished product

--- javascript.js
+++ javascript.js
@@ -1,1 +1,34 @@
-console.log("test");
+const screenContainer = document.querySelector(".screen-container");
+const resizeButton = document.createElement("button");
+resizeButton.textContent = "Resize me!";
+const buttonDiv = document.querySelector(".button-div");
+buttonDiv.appendChild(resizeButton);
+let square = 256;
+
+function createGrid() {
+  for (let i = 0; i < square; i++) {
+    const squares = document.createElement("div");
+    squares.className = "squares";
+    screenContainer.appendChild(squares);
+    squares.style.backgroundColor = "white";
+  }
+
+  let createdSquares = document.querySelectorAll(".squares");
+
+  createdSquares.forEach((square) => {
+    square.addEventListener("mouseenter", function () {
+      square.style.backgroundColor = "red";
+    });
+  });
+}
+createGrid();
+
+resizeButton.addEventListener("click", function () {
+  let getUserNumber = prompt("Enter the number of squares in your new grid:");
+  square = getUserNumber * getUserNumber;
+  let oldSquares = document.querySelectorAll(".squares");
+  oldSquares.forEach((square) => {
+    square.remove();
+  });
+  createGrid();
+});
